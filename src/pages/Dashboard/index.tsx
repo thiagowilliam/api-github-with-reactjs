9d8506b1ce1524ff2071cc4bcacf87bf1f5f4fb2
thiago william <thiago.will.silva@gmail.com>
2020-04-21 17:05:04 +0100

    front-end project

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,102 @@
+import React, { useState, FormEvent, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/img/logo.svg';
+
+import * as S from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositorio');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositorio');
+    }
+  }
+
+  return (
+    <>
+      <S.LogoGitExplore src={logoImg} alt="Github Explore" />
+      <S.Title>Explore repositórios no Github</S.Title>
+
+      <S.Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositorio"
+        />
+        <button type="submit">Pesquisar</button>
+      </S.Form>
+
+      {inputError && <S.Error>{inputError}</S.Error>}
+
+      <S.Repositories>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <S.TextRepositories>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </S.TextRepositories>
+
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </S.Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
